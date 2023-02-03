43907de0df5e5aa92a1f2929ebd270cb05474f08
kklxskldnijd <zakaabdi0251@gmail.com>
2023-02-03 13:06:41 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+def my_function(fname):
+   print((fname) + "")
+
+my_function("fra - ronaldo: makker er det dig")
+my_function("fra - messi: nej hvem er du din taber")
+my_function("fra - ronaldo: du er heldig du har et meget bedre hold")
+my_function("fra - messi: har du ondt i røven, jeg vandt fordi jeg er den bedste i hele verden punktum")
+my_function("fra - messi: tude fjæs, tude fjæs")
+my_function("fra - messi nej hvem er du din taber")
+my_function("fra - bot: jeg træt af jeres skinderi stop ellers banner jeg jer begge i hvert eneste sociale medie")
